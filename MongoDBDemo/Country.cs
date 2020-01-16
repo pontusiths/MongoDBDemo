ea5619492711124d5b89aa3427358e129111031f
Pontus <pontus.lindgren@iths.se>
2020-01-16 10:31:39 +0100

    Added demo

---
+++ MongoDBDemo/Country.cs
@@ -0,0 +1,12 @@
+﻿using MongoDB.Bson;
+
+namespace MongoDBDemo
+{
+    public class Country
+    {
+        public ObjectId _id { get; set; }
+        public int Population { get; set; }
+        public bool Exists { get; set; }
+        public string Name{ get; set; }
+    }
+}
---
+++ MongoDBDemo/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using MongoDB.Driver;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization;
+
+namespace MongoDBDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MongoClient mongoClient = new MongoClient("mongodb://localhost:27017");
+            var db = mongoClient.GetDatabase("countriesdb");
+            var collection = db.GetCollection<BsonDocument>("countries");
+            var filter = Builders<BsonDocument>.Filter.Eq("Name", "Sweden");
+            BsonDocument doc = collection.Find<BsonDocument>(filter).FirstOrDefault();
+            Country sweden = BsonSerializer.Deserialize<Country>(doc);
+
+            Country norway = new Country
+            {
+                _id = new ObjectId(),
+                Exists = true,
+                Name = "norway",
+                Population = 4000000
+            };
+
+            //Country norway = new Country {
+            //    _id = 2,
+            //    Exists = true,
+            //    Name = "norway",
+            //    Population = 4000000
+            //};
+
+            collection.InsertOne(norway.ToBsonDocument());
+
+
+
+
+
+
+
+
+
+
+            //var filter = Builders<BsonDocument>.Filter.Eq("class", "Mammalia");
+            //var carnivoraFilter = Builders<BsonDocument>.Filter.Eq("color" , "black");
+            //var andFilter = Builders<BsonDocument>.Filter.And(filter, carnivoraFilter);
+            //var found = collection.Find(andFilter);
+            //// Använd .ToEnumerable() för att iterera över resultatet.
+            //foreach (var x in found.ToEnumerable())
+            //{
+            //    Console.WriteLine(x.ToString());
+            //}
+        }
+    }
+}
